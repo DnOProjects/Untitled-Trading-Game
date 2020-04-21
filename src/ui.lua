1c12d6d66d5524db21fc1460074e4ff6c2b2c09a
Relton <ori.taylor@gmail.com>
2020-04-21 20:03:12 +0100

    Adding fonts

--- src/ui.lua
+++ src/ui.lua
@@ -20,7 +20,7 @@ end
 
 function initUI()
 	addBackgroundImage({1, 2}, images.shipBackground)
-	addPrint({1, 2}, "Tropical Trader", 0, 50, 1920, 100, 0, 0.1, 0.15, 1, "center")
+	addPrint({1, 2}, "Tropical Trader", 0, 50, 1920, 150, 0, 0.1, 0.15, 1, "center")
 
 	addButton(1, "inGame", "Play", 200, 250, 500, 140, 70, 0.1, 0.1, 0.1, 0.6)
 	addButton(1, 2, "Options", 200, 450, 500, 140, 70, 0.1, 0.1, 0.1, 0.6)
@@ -35,17 +35,17 @@ function addBackgroundImage(pages, image)
 end
 
 function addButton(page, pageToGo, text, x, y, width, height, textSize, r, g, b, a)
-	buttons[#buttons+1] = {page = page, pageToGo = pageToGo, text = text, x = x, y = y, width = width, height = height, textSize = textSize, color = {r, g, b, a}, mouseOver = false}
+	buttons[#buttons+1] = {page = page, pageToGo = pageToGo, text = text, x = x, y = y, width = width, height = height, textSize = textSize, font = "IMMORTAL", color = {r, g, b, a}, mouseOver = false}
 end
 
 function addSlider(page, text, x, y, width, height, textSize, r, g, b, a, value, sliderWidth, sliderHeight)
 	-- sliderWidth must be even as it is halved
 	-- sliderHeight must be odd as the 3 from the slider line is odd
-	sliders[#sliders+1] = {page = page, text = text, x = x, y = y, width = width, height = height, textSize = textSize, color = {r, g, b, a}, value = value, sliderWidth = sliderWidth, sliderHeight = sliderHeight}
+	sliders[#sliders+1] = {page = page, text = text, x = x, y = y, width = width, height = height, textSize = textSize, font = "IMMORTAL", color = {r, g, b, a}, value = value, sliderWidth = sliderWidth, sliderHeight = sliderHeight}
 end
 
 function addPrint(pages, text, x, y, limit, textSize, r, g, b, a, align)
-	prints[#prints+1] = {pages = pages, text = text, x = x, y = y, limit = limit, textSize = textSize, color = {r, g, b, a}, align = align}
+	prints[#prints+1] = {pages = pages, text = text, x = x, y = y, limit = limit, textSize = textSize, font = "TropicalAsian", color = {r, g, b, a}, align = align}
 end
 
 -- Updates
@@ -97,7 +97,7 @@ end
 
 -- Drawing
 function ui.draw()
-	drawButtons()	
+	drawButtons()
 	drawSliders()
 	drawPrints()
 end
@@ -120,7 +120,7 @@ function drawButtons()
 			end
 			love.graphics.rectangle("fill", button.x, button.y, button.width, button.height)
 			love.graphics.setColor(1, 1, 1, 0.95)
-			love.graphics.setFont(love.graphics.newFont(button.textSize))
+			love.graphics.setFont(love.graphics.newFont("fonts/"..button.font..".ttf", button.textSize))
 			love.graphics.printf(button.text, button.x, button.y + (button.height/2) - (button.textSize/1.8), button.width, "center")
 		end
 	end
@@ -132,7 +132,7 @@ function drawSliders()
 		    love.graphics.setColor(slider.color)
 		    love.graphics.rectangle("fill", slider.x, slider.y, slider.width, slider.height)
 		    love.graphics.setColor(1, 1, 1)
-		    love.graphics.setFont(love.graphics.newFont(slider.textSize))
+		    love.graphics.setFont(love.graphics.newFont("fonts/"..slider.font..".ttf", slider.textSize))
 		    love.graphics.printf(slider.text.." : "..slider.value, slider.x, slider.y + slider.height/2 - slider.textSize/1.5, slider.width, "center")
 			love.graphics.rectangle("line", slider.x+20, slider.y+slider.height-20, slider.width-40, 3)
 			love.graphics.rectangle("fill", slider.x+20+((slider.width-40)/100)*slider.value-slider.sliderWidth/2, slider.y+slider.height-(((slider.sliderHeight-3)/2)+20), slider.sliderWidth, slider.sliderHeight)
@@ -143,7 +143,7 @@ end
 function drawPrints()
 	for i, printText in pairs(prints) do
 		if (logic.inList(currentPage, printText.pages)) then
-			love.graphics.setFont(love.graphics.newFont(printText.textSize))
+			love.graphics.setFont(love.graphics.newFont("fonts/"..printText.font..".ttf", printText.textSize))
 			love.graphics.setColor(printText.color)
 			love.graphics.printf(printText.text, printText.x, printText.y, printText.limit, printText.align)
 			love.graphics.setColor(1, 1, 1)
